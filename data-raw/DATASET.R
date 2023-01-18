45e2516ef4a398c21c0c099044f8195821c7605a
zemw <19867783+zemw@users.noreply.github.com>
2023-01-19 10:56:24 +1100

    Remove data-raw

--- data-raw/DATASET.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `DATASET` dataset goes here
-
-usethis::use_data(DATASET, overwrite = TRUE)
-
-testdata = readr::read_file("data-raw/testdata.m")
-usethis::use_data(testdata, internal = T)
